02cabadd61263fdce5de8e9866b4d7c15179d095
Yati Sagade <yati.sagade@gmail.com>
2016-12-29 23:06:21 +0100

    Recursive backfilling of command files in a directory

--- training/backfill_velocities.py
+++ training/backfill_velocities.py
@@ -6,8 +6,11 @@ the motion module's inner workings. To be clear, each output row is
 
 where both velocity values represent the speed _before_ executing the command
 (which implies the velocities in the first entry in a file are always zero."""
-import argparse
+import os
 import sys
+import argparse
+import tempfile
+import shutil
 
 
 def read(filename):
@@ -20,12 +23,14 @@ def read(filename):
             ret[key] = value
     return sorted(ret.items(), key=lambda t: t[0])
 
+
 def _clamp(x):
     if x < -255:
         return -255
     if x > 255:
         return 255
     return x
+
 
 def _update_velocities(cmd, vels):
     step_size = 3
@@ -46,19 +51,45 @@ def _update_velocities(cmd, vels):
         vels[0] = vels[1] = sum(vels) / 2
 
 
-def main(cmdfile, outfile):
+def process_file(cmdfile, outfile):
+    if outfile is None:
+        outfile = cmdfile
+    fd, fname = tempfile.mkstemp()
+    os.close(fd)
     cmds = read(cmdfile)
     velocities = [0, 0] # left, right
-    with open(outfile, "wb") as out:
+    with open(fname, "wb") as out:
         for timestamp, cmd in cmds:
             out.write("{},{},{},{}\n".format(timestamp, cmd, *velocities))
             _update_velocities(cmd, velocities)
+    shutil.move(fname, outfile)
+
+
+def main(cmdfile, outfile, recursive):
+    if recursive:
+        if not os.path.isdir(cmdfile):
+            raise Exception("When --recursive is given, cmdfile "
+                            "must be a directory.")
+
+        for dirpath, _, filenames in os.walk(cmdfile):
+            for filename in filenames:
+                if filename == "commands.txt":
+                    process_file(os.path.join(dirpath, filename), None)
+    else:
+        process_file(cmdfile, outfile)
+    return 0
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("cmdfile", help="Input command file")
-    parser.add_argument("outfile", help="Output (new-format) command file")
-
+    parser.add_argument("-o", "--output",
+                        type=str, help="Output (new-format) command file")
+    parser.add_argument("-r", "--recursive",
+                        action="store_true",
+                        help="Recursive processing. If given, "
+                        "cmdfile must be a directory. Any file called "
+                        "commands.txt in any of the subdirectories is processed"
+                        " in place. -o is ignored here.")
     args = parser.parse_args()
-    sys.exit(main(args.cmdfile, args.outfile))
+    sys.exit(main(args.cmdfile, args.output, args.recursive))
