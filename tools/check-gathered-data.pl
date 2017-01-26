9647b074a7c6c623c06cea3afa032b6106c61bd5
Yati Sagade <yati.sagade@gmail.com>
2017-01-26 20:10:55 +0100

    modify check-gathered-data.pl to account for the new kind of training data

--- tools/check-gathered-data.pl
+++ tools/check-gathered-data.pl
@@ -6,7 +6,6 @@ use Carp       qw(confess);
 use Pod::Usage qw(pod2usage);
 use File::Spec;
 
-my @labels   = (0..6);
 my $ok_tuple = {status => "ok"};
 
 MAIN: {
@@ -14,8 +13,7 @@ MAIN: {
     my $dirname = shift @ARGV or pod2usage();
 
     my %checks = (
-        "label directories present" => \&_assert_label_dirs_present,
-        "labeldir content check"    => \&_check_labeldir_contents,
+        "speedfile content check"    => \&_check_speedfile_contents,
     );
 
     while (my ($check_name, $check_func) = each %checks) {
@@ -26,75 +24,58 @@ MAIN: {
         }
     }
 
-    _show_labeldir_stats($dirname);
+    _show_stats($dirname);
 }
 
-sub _show_labeldir_stats {
-    my $dirname = shift;
+sub _is_img_filename {
+    my $filename = shift;
+    my ($extn)   = ($filename =~ /\.([^.]+)$/);
+    return $extn && ($extn =~ /^(?:jpg|jpeg|png|bmp)$/i);
+}
+
+sub _show_stats {
+    my $path = shift;
 
     my $total_count      = 0;
     my $total_file_count = 0;
 
-    for my $label (@labels) {
-        my $path = File::Spec->catfile($dirname, $label);
-        confess "$path does not name a directory." unless (-d $path);
-        my $speedfile = File::Spec->catfile($path, "speeds.txt");
-        confess "$speedfile does not name a file." unless (-f $speedfile);
+    my $speedfile = File::Spec->catfile($path, "speeds.txt");
+    confess "$speedfile does not name a file." unless (-f $speedfile);
 
-        my $count = do {
-            open my $in, "<:encoding(utf8)", $speedfile or
-                confess "Could not open $speedfile for reading: $!";
-            my $ctr = 0;
-            $ctr++ while (<$in>);
-            close $in;
-            $ctr;
-        };
-        $total_count += $count;
+    my $count = do {
+        open my $in, "<:encoding(utf8)", $speedfile or
+            confess "Could not open $speedfile for reading: $!";
+        my $ctr = 0;
+        $ctr++ while (<$in>);
+        close $in;
+        $ctr;
+    };
+    $total_count += $count;
 
-        my $file_count = do {
-            opendir my $dh, $path or confess "Could not open directory $path: $!";
-            my $ctr = 0;
-            while (readdir $dh) {
-                next if (/^\./ ||
-                         ! -f File::Spec->catfile($path, $_) ||
-                         /^speeds\.txt$/);
-                ++$ctr;
-            }
-            closedir $dh;
-            $ctr;
-        };
-        $total_file_count += $file_count;
-
-        if ($file_count == $count) {
-            say "Label $label: $count examples in $path.";
-        } else {
-            say "Label $label: $count examples as per speedfile, ".
-                "but $file_count files in $path.";
+    my $file_count = do {
+        opendir my $dh, $path or confess "Could not open directory $path: $!";
+        my $ctr = 0;
+        while (readdir $dh) {
+            next if (/^\./ || 
+                     !-f File::Spec->catfile($path, $_) ||
+                     !_is_img_filename($_));
+            ++$ctr;
         }
+        closedir $dh;
+        $ctr;
+    };
+    if ($file_count == $count) {
+        say "Total $count examples in $path.";
+    } else {
+        say "error: there are $count examples as per speedfile, but ".
+            "$file_count files in $path.";
     }
-    say "Total count:\n\tas per speedfiles: $total_count\n\t".
-        "images in all labeldirs: $total_file_count";
-}
-
-sub _check_labeldir_contents {
-    my $dirname = shift;
-    my @errors;
-    for my $label (@labels) {
-        my $labeldir = File::Spec->catfile($dirname, $label);
-        confess "$labeldir does not name a directory." unless(-d $labeldir);
-        my $speedfile = File::Spec->catfile($labeldir, "speeds.txt");
-        my $ret = _check_speedfile_contents($labeldir, $speedfile);
-        push @errors, $ret->{error} if ($ret->{status} eq "error");
-    }
-    if (@errors) {
-        return mkerr(join ";", @errors);
-    }
-    return $ok_tuple;
 }
 
 sub _check_speedfile_contents {
-    my ($dirname, $speedfile) = @_;
+    my $dirname = shift;
 
+    my $speedfile = File::Spec->catfile($dirname, "speeds.txt");
     open my $in, "<:encoding(utf8)", $speedfile or
         confess "Could not open $speedfile for reading: $!";
 
@@ -102,7 +83,7 @@ sub _check_speedfile_contents {
     my %entry_counts;
     while (<$in>) {
         chomp;
-        my ($filename, $lspeed, $rspeed) = split /,/;
+        my ($filename, $lspeed, $rspeed, $lspeed_aft, $rspeed_aft) = split /,/;
         my $path = File::Spec->catfile($dirname, $filename);
         push @missing_files, $filename unless (-f $path);
         $entry_counts{$filename}++;
@@ -122,35 +103,9 @@ sub _check_speedfile_contents {
     while (my ($msg, $vals) = each %msg_val_map) {
         push @errors, _err_msg_csv($msg, @$vals) if (@$vals);
     }
-
     if (@errors) {
         return mkerr(join ";", @errors);
     }
-
-    return $ok_tuple;
-}
-
-sub _assert_label_dirs_present {
-    my $dirname = shift;
-
-    opendir my $dh, $dirname or
-        confess "Could not open directory $dirname: $!";
-
-    my %missing = map { $_ => 1 } @labels;
-    while (readdir $dh) {
-        next if (/^\./ || !/^\d+$/);
-        delete $missing{$_};
-    }
-
-    closedir $dh;
-    if (scalar keys %missing) {
-        my $missing_dirs = join ",",
-                                map { "'$_'" }
-                                map { File::Spec->catfile($dirname, $_) }
-                                keys %missing;
-        return mkerr("Missing label directories: $missing_dirs");
-    }
-
     return $ok_tuple;
 }
 
