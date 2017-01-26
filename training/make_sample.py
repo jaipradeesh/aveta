0c90a1f70ad7c5b653d2bbecc733d84d8a209be2
Yati Sagade <yati.sagade@gmail.com>
2017-01-26 21:24:04 +0000

    Change make_sample.py to use the new format

--- training/make_sample.py
+++ training/make_sample.py
@@ -5,6 +5,7 @@ import sys
 import shutil
 import tempfile
 import argparse
+import random
 
 import train_test_split
 
@@ -15,56 +16,38 @@ def parse_args():
     parser.add_argument("--fraction", type=float,
                         help="fraction of data(between 0 and 1)",
                         default=0.1)
-    parser.add_argument("--test_fraction", type=float,
-                        help="test set proportion, as a floating point number "
-                             "between 0 and 1.",
-                        default=0.1)
-
-    parser.add_argument("--valid_fraction", type=float,
-                        help="validation set proportion, as a floating point number "
-                             "between 0 and 1.",
-                        default=0.1)
     args = parser.parse_args()
     return args
 
-
-def process_label_dir(indir, outdir, frac):
-    speedfile = os.path.join(indir, "speeds.txt")
-    entries = train_test_split.read_speedfile(speedfile)
-    count = int(frac * len(entries))
-    with open(os.path.join(outdir, "speeds.txt"), "w") as fp:
-        for entry in entries[:count]:
-            fp.write("{}\n".format(",".join(entry)))
-            shutil.copy(os.path.join(indir, entry[0]),
-                        os.path.join(outdir, entry[0]))
-
-
-def main(input_dir, output_dir, frac, test_frac, valid_frac):
+def main(input_dir, output_dir, frac):
     if not os.path.exists(input_dir) or not os.path.isdir(input_dir):
         print("Error: {} does not name a directory")
         return 1
 
     if os.path.exists(output_dir):
         shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
 
-    tmp_output_dir = tempfile.mkdtemp()
-    labels = train_test_split.labelnames(input_dir)
+    speedfile = os.path.join(input_dir, "speeds.txt")
+    names = []
+    lines = []
+    print speedfile
+    with open(speedfile) as fp:
+        for line in fp:
+            name, _ = line.strip().split(",", 1)
+            names.append(name)
+            lines.append(line.strip())
+    
+    n = int(frac * len(names))
+    if not n:
+        return 0
 
-    for label in labels:
-        os.makedirs(os.path.join(tmp_output_dir, label))
-        process_label_dir(os.path.join(input_dir, label),
-                          os.path.join(tmp_output_dir, label),
-                          frac)
-
-    try:
-        ret = train_test_split.main(tmp_output_dir,
-                                    output_dir,
-                                    test_frac,
-                                    valid_frac)
-
-        return ret
-    finally:
-        shutil.rmtree(tmp_output_dir)
+    choice = random.sample(range(len(names)), n)
+    with open(os.path.join(output_dir, "speeds.txt"), "wb") as fp:
+        for i in choice:
+            fp.write("{}\n".format(lines[i]))
+            shutil.copy(os.path.join(input_dir, names[i]),
+                        os.path.join(output_dir, names[i]))
 
     return 0
 
@@ -72,9 +55,7 @@ if __name__ == "__main__":
     args = parse_args()
     sys.exit(main(args.input_dir,
                   args.output_dir,
-                  args.fraction,
-                  args.test_fraction,
-                  args.valid_fraction))
+                  args.fraction))
 
 
 
