845ba4e6e584d5a2108bcdfb0cea598fb75dd238
Yati Sagade <yati.sagade@gmail.com>
2017-01-29 00:34:01 +0100

    prediction server

--- predictsrv.py
+++ predictsrv.py
@@ -14,6 +14,7 @@
         bit 0 is set to indicate quitting.
 
 """
+import cv2
 import os
 import sys
 import socket
@@ -21,9 +22,11 @@ import argparse
 import struct
 
 import numpy as np
+from scipy.misc import imresize
 from StringIO import StringIO
 from PIL import Image
 from matplotlib import pyplot as plt
+from keras.models import load_model
 
 from network import read_n_strict
 
@@ -37,35 +40,45 @@ def read_input_msg(stream):
     if flags & 0x1:
         raise StopIteration
 
-    img_bytes = read_n_strict(img_size)
+    img_bytes = read_n_strict(stream, img_size)
+    print("Read {} image bytes.".format(img_size))
     fp = StringIO(img_bytes)
     img = Image.open(fp)
     imarr = np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
-
+    imarr = np.fliplr(np.flipud(imarr))
+    h, w, _ = imarr.shape
+    imarr = imarr[h/2:,:]
     print(imarr.shape)
-
     return imarr, left_speed, right_speed
 
+
+model_path = "models/aveta-model.hdf5"
 
 def main(addr):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(addr)
     sock.listen(1)
 
+    model = load_model(model_path)
     while True:
         conn, cli = sock.accept()
-        handle_conn(conn)
+        handle_conn(conn, model)
 
 
-def handle_conn(conn):
+def handle_conn(conn, model):
     fmt = "<Bhh"
     while True:
         try:
-            img, lspeed, rspeed = read_input_msg(conn)
-            conn.sendall(struct.pack(fmt, 0x1, 0, 0))
+            img_, lspeed, rspeed = read_input_msg(conn)
+            img = imresize(img_, (150, 400))
+            img = np.rollaxis(img, 2, 0)
+            X = [np.array([img]), np.array([[lspeed, rspeed]])]
+            lout, rout = model.predict(X).reshape(2,)
+            print lout, rout
+            conn.sendall(struct.pack(fmt, 0x00, lout, rout))
         except StopIteration:
             return
-        conn.sendall(struct.pack(fmt, 0, lspeed, rspeed))
+        conn.sendall(struct.pack(fmt, 0x01, lspeed, rspeed))
 
 
 def parse_args():
